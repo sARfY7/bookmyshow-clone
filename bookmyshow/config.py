1d202a9ede897a0665126648b00ad48b7ae5c2bb
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-12-21 08:00:47 +0530

    tmdb api key added

--- bookmyshow/config.py
+++ bookmyshow/config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-# "postgres+psycopg2://postgres:password@localhost/bookmyshow"
 class Config:
   SQLALCHEMY_DATABASE_URI = environ.get('DB_URI')
   SECRET_KEY = environ.get('SECRET_KEY')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  TMDB_API_KEY = environ.get("TMDB_API_KEY")
