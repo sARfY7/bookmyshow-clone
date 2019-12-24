fb8c78214c7dd5ed35668fca7522ce90b1fafd82
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-12-24 18:24:50 +0530

    elastic search added

--- bookmyshow/__init__.py
+++ bookmyshow/__init__.py
@@ -2,12 +2,14 @@ from bookmyshow.config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from elasticsearch import Elasticsearch
 
 # Initialize App and App Extensions
 app = Flask(__name__) # Creating a new Flask App Instance
 app.config.from_object(Config) # Add configuration to Flask App Instance
 db = SQLAlchemy(app) # Creating a new Flask-SQLAlchemy instance by passing in Flask App Instance
 bcrypt = Bcrypt(app) # Creating a new Flask-Bcrypt instance by passing in Flask App Instance
+es = Elasticsearch()
 
 # Models Import
 import bookmyshow.models
