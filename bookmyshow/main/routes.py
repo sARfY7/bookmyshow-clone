023992e1746b1c5adc40faa19ec6ce0216efafc4
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-12-24 18:24:41 +0530

    search route added

--- bookmyshow/main/routes.py
+++ bookmyshow/main/routes.py
@@ -3,7 +3,7 @@ from bookmyshow.models import MovieScreening, Movie, Theatre, Booking, Seat
 from bookmyshow import db
 import requests
 from bookmyshow.auth import user_login_required
-from .services import get_in_theatre_movies, get_movie_screenings, get_unavailable_seats, get_booking_summary, get_booking_confirmation
+from .services import get_in_theatre_movies, get_movie_screenings, get_unavailable_seats, get_booking_summary, get_booking_confirmation, get_search_query_result
 
 main = Blueprint("main", __name__)
 
@@ -20,24 +20,31 @@ def home():
     movies, total_movies = get_in_theatre_movies(offset)
     return render_template("home.html", movies=movies, total_pages=total_movies)
 
-@main.route("/movie/<int:movie_id>/book")
+@main.route("/movies/<int:movie_id>/book")
 def book_movie(movie_id):
     movie, screening_theates = get_movie_screenings(movie_id)
     return render_template("booking.html", movie=movie, theatres=screening_theates)
 
-@main.route("/movie/<int:movie_id>/theatre/<int:theatre_id>/booking")
+@main.route("/movies/<int:movie_id>/theatres/<int:theatre_id>/booking")
 def select_seat(movie_id, theatre_id):
   movie, theatre, unavailable_seats = get_unavailable_seats(movie_id, theatre_id)
   return render_template("select-seat.html", movie=movie, theatre=theatre, unavailable_seats=unavailable_seats)
 
-@main.route("/movie/<int:movie_id>/theatre/<int:theatre_id>/summary", methods=["POST"])
+@main.route("/movies/<int:movie_id>/theatres/<int:theatre_id>/summary", methods=["POST"])
 def summary(movie_id, theatre_id):
   movie, theatre, selected_seats, total_amount = get_booking_summary(movie_id, theatre_id, request.form["seats"])
   return render_template("summary.html", movie=movie, theatre=theatre, seats=selected_seats, total_amount=total_amount)
 
 
-@main.route("/movie/<int:movie_id>/theatre/<int:theatre_id>/confirmation", methods=["POST"])
+@main.route("/movies/<int:movie_id>/theatres/<int:theatre_id>/confirmation", methods=["POST"])
 @user_login_required
 def confirmation(movie_id, theatre_id):
   movie, theatre, new_booking, booked_seats = get_booking_confirmation(movie_id, theatre_id, request.form["seats"].replace("'", '"'), request.form["amount"])
   return render_template("user/confirmation.html", movie=movie, theatre=theatre, booking=new_booking, seats=booked_seats)
+
+@main.route("/movies/search")
+def search():
+  query = request.args.get("q")
+  search_result = get_search_query_result(query)
+  print(search_result)
+  return render_template("search-result.html", result=search_result)
