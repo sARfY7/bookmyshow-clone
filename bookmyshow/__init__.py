b4e20aa08dbe55d07187edba940f5dcc8a1a6ca8
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-12-27 17:33:45 +0530

    elastic search removed in production env

--- bookmyshow/__init__.py
+++ bookmyshow/__init__.py
@@ -2,15 +2,18 @@ from bookmyshow.config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from elasticsearch import Elasticsearch
 from celery import Celery
+import os
+if (os.environ.get("FLASK_ENV") == "development"):
+    from elasticsearch import Elasticsearch
 
 # Initialize App and App Extensions
 app = Flask(__name__) # Creating a new Flask App Instance
 app.config.from_object(Config) # Add configuration to Flask App Instance
 db = SQLAlchemy(app) # Creating a new Flask-SQLAlchemy instance by passing in Flask App Instance
 bcrypt = Bcrypt(app) # Creating a new Flask-Bcrypt instance by passing in Flask App Instance
-es = Elasticsearch() # Creating Elasticsearch intance for searching
+if (os.environ.get("FLASK_ENV") == "development"):
+    es = Elasticsearch() # Creating Elasticsearch intance for searching
 celery = Celery(app.name, broker="redis://localhost:6379/0")
 celery.conf.update(
     task_track_started=True,
