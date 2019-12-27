e472a764c83fb0da0c67a2b920b139c28935604e
Sarfaraz Ahmed <imsarfy@gmail.com>
2019-12-27 14:23:18 +0530

    celery config added

--- bookmyshow/config.py
+++ bookmyshow/config.py
@@ -5,3 +5,5 @@ class Config:
   SECRET_KEY = environ.get('SECRET_KEY')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   TMDB_API_KEY = environ.get("TMDB_API_KEY")
+  CELERY_BROKER_URL = environ.get("CELERY_BROKER_URL")
+  CELERY_RESULT_BACKEND = environ.get("CELERY_RESULT_BACKEND")
